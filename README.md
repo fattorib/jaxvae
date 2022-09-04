dd9e293eb28d5f18ff4fc934ba268b228a372454
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-04 17:35:55 -0400

    todos + VAE base class

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # jaxVAE
 
 VAE in Jax
+
+TODO:
+
+1. Model (enc + dec)
+2. Training loop
+3. Logging with aim (including samples)
+
---
+++ src/models/VAE.py
@@ -0,0 +1,51 @@
+from typing import Any, Callable, Sequence
+from jax import numpy as jnp
+from flax import linen as nn
+from functools import partial
+
+ModuleDef = Any
+dtypedef = Any
+
+class VAE(nn.Module):
+
+    """Convolutional Variational Autoencoder as described in PAPER
+
+    Model configuration taken from https://www.tensorflow.org/tutorials/generative/cvae
+    """
+    num_latents: int = 5
+    dtype: dtypedef = jnp.float32
+    
+    # # define init for conv layers. Do we need this?
+    # kernel_init: Callable = nn.initializers.normal(stddev=0.02, dtype=dtype)
+
+    @nn.compact
+    def __call__(self, x, z = None):
+        
+        # encoder: learning $q_\phi (z|x)$ where x is input image and z is the latent
+        if z is not None:
+            x = nn.Conv(kernel_size=(3,3), strides=(2,2), features = 32)(x)
+            x = nn.relu(x)
+            x = nn.Conv(kernel_size=(3,3), strides=(2,2), features = 64)(x)
+            x = nn.relu(x)
+            x = jnp.mean(x, axis=(1, 2))
+            z = nn.Dense(features= 2 * self.num_latents)
+
+        # decoder: learning $p_\theta (x|z)$ where z is a latent and x is a generated sample
+        z = nn.Dense(features = 7*7*32)(z)
+        z = z.reshape(7,7,32)
+        z = nn.ConvTranspose(kernel_size=(3,3), strides=(2,2), features = 64, padding = 'SAME')(z)
+        z = nn.relu(z)
+        z = nn.ConvTranspose(kernel_size=(3,3), strides=(2,2), features = 32, padding = 'SAME')(z)
+        z = nn.relu(z)
+
+        z = nn.ConvTranspose(kernel_size=(3,3), strides=(1,1), features = 1, padding = 'SAME')(z)
+
+        return z 
+
+
+
+
+
+
+
+
