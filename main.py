14d81b2edef84794471ae618a82fa01f96de9d2f
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-05 16:12:48 -0400

    repo organization

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import numpy as np
-import optax
-
 from jax import random
 
 # Flax imports
-from flax.training import train_state
-import jax.numpy as jnp
 import jax
 
 # PyTorch - for dataloading
@@ -17,8 +13,9 @@ from src.utils.dataloader import *
 from src.models.VAE import VAE
 
 # Utils
-from src.training.train_utils import *
+from src.training.losses import *
 from src.utils.sample import sample_from_latents, np_to_fig
+from src.training.training_utils import initialized, create_train_state
 
 # Logging/Config Stuffs
 import argparse
@@ -191,39 +188,6 @@ def main():
             step=epoch,
             context={"subset": "validation"},
         )
-
-
-def initialized(key, image_size, model):
-    input_shape = (1, image_size, image_size, 1)
-
-    @jax.jit
-    def init(rng, shape):
-        return model.init(rng, shape, rng)
-
-    variables = init(rng=key, shape=jnp.ones(input_shape))
-    return variables
-
-
-def create_train_state(
-    rng, learning_rate_fn, weight_decay, model, grad_accum_steps
-):
-    """Creates initial `TrainState`."""
-    params = initialized(rng, 28, model)
-    mask = jax.tree_map(lambda x: x.ndim != 1, params)
-    tx = optax.adamw(
-        learning_rate=learning_rate_fn, weight_decay=weight_decay, mask=mask
-    )
-
-    if grad_accum_steps > 1:
-        tx = optax.MultiSteps(tx, every_k_schedule=grad_accum_steps)
-
-    state = train_state.TrainState.create(
-        apply_fn=model.apply,
-        params=params,
-        tx=tx,
-    )
-    return state
-
 
 @jax.jit
 def train_step(state, batch, rng_key):
---
+++ src/training/training_utils.py
@@ -0,0 +1,48 @@
+""" 
+Helper methods used during training setup. 
+"""
+import jax 
+import jax.numpy as jnp
+import optax
+from flax.training import train_state
+
+def initialized(key, image_size, model):
+    """Initializes param dict for a model
+
+    Args:
+        key (_type_): _description_
+        image_size (_type_): _description_
+        model (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    input_shape = (1, image_size, image_size, 1)
+
+    @jax.jit
+    def init(rng, shape):
+        return model.init(rng, shape, rng)
+
+    variables = init(rng=key, shape=jnp.ones(input_shape))
+    return variables
+
+
+def create_train_state(
+    rng, learning_rate_fn, weight_decay, model, grad_accum_steps
+):
+    """Creates initial `TrainState` for model."""
+    params = initialized(rng, 28, model)
+    mask = jax.tree_map(lambda x: x.ndim != 1, params)
+    tx = optax.adamw(
+        learning_rate=learning_rate_fn, weight_decay=weight_decay, mask=mask
+    )
+
+    if grad_accum_steps > 1:
+        tx = optax.MultiSteps(tx, every_k_schedule=grad_accum_steps)
+
+    state = train_state.TrainState.create(
+        apply_fn=model.apply,
+        params=params,
+        tx=tx,
+    )
+    return state
