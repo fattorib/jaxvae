6e89dbbef3be959debd82fefb8f13324c004834a
Ben <ben.fattorri@mail.utoronto.ca>
2022-09-06 12:10:21 -0400

    back to conv

--- src/models/VAE.py
+++ src/models/VAE.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Callable
 from jax import numpy as jnp
 from flax import linen as nn
 import jax
@@ -15,68 +15,83 @@ def reparametrize(mean, logvar, rng_key):
     return z, (mean, logvar)
 
 
-# class Decoder(nn.Module):
-#     @nn.compact
-#     def __call__(self, z):
-
-#         # decoder: learning $p_\theta (x|z)$ where z is a latent and x is a generated sample
-#         z = nn.Dense(features=7 * 7 * 32)(z)
-#         z = z.reshape(-1, 7, 7, 32)
-#         z = nn.relu(z)
-#         z = nn.ConvTranspose(
-#             kernel_size=(3, 3), strides=(2, 2), features=64, padding="SAME"
-#         )(z)
-#         z = nn.relu(z)
-#         z = nn.ConvTranspose(
-#             kernel_size=(3, 3), strides=(2, 2), features=32, padding="SAME"
-#         )(z)
-#         z = nn.relu(z)
-
-#         out = nn.ConvTranspose(
-#             kernel_size=(3, 3), strides=(1, 1), features=1, padding="SAME"
-#         )(z)
-
-#         return out
-
-
-# class Encoder(nn.Module):
-#     num_latents: int
-
-#     @nn.compact
-#     def __call__(self, x):
-
-#         # encoder: learning $q_\phi (z|x)$ where z is the generated latent and x is a datapoint
-#         x = nn.Conv(kernel_size=(3, 3), strides=(2, 2), features=32)(x)
-#         x = nn.relu(x)
-#         x = nn.Conv(kernel_size=(3, 3), strides=(2, 2), features=64)(x)
-#         x = nn.relu(x)
-#         x = jnp.mean(x, axis=(1, 2))
-#         z_mean = nn.Dense(features=self.num_latents)(x)
-#         z_logvar = nn.Dense(features=self.num_latents)(x)
-#         return z_mean, z_logvar
-
-
 class Decoder(nn.Module):
+
+    kernel_init: Callable = nn.initializers.normal(stddev=0.01,)
+    bias_init: Callable = nn.initializers.normal(stddev=0.01,)
+
     @nn.compact
     def __call__(self, z):
+
         # decoder: learning $p_\theta (x|z)$ where z is a latent and x is a generated sample
-        z = nn.Dense(features=500, name="fc1")(z)
+        z = nn.Dense(features=7 * 7 * 32)(z)
+        z = z.reshape(-1, 7, 7, 32)
         z = nn.relu(z)
-        out = nn.Dense(784, name="fc2")(z)
+        z = nn.ConvTranspose(
+            kernel_size=(3, 3), strides=(2, 2), features=64, padding="SAME", kernel_init=self.kernel_init, bias_init=self.bias_init
+        )(z)
+        z = nn.relu(z)
+        z = nn.ConvTranspose(
+            kernel_size=(3, 3), strides=(2, 2), features=32, padding="SAME", kernel_init=self.kernel_init, bias_init=self.bias_init
+        )(z)
+        z = nn.relu(z)
+
+        out = nn.ConvTranspose(
+            kernel_size=(3, 3), strides=(1, 1), features=1, padding="SAME", kernel_init=self.kernel_init, bias_init=self.bias_init
+        )(z)
 
         return out
 
 
 class Encoder(nn.Module):
-    num_latents: int = 5
+    num_latents: int
+    kernel_init: Callable = nn.initializers.normal(stddev=0.01,)
+    bias_init: Callable = nn.initializers.normal(stddev=0.01,)
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Dense(500, name="fc1")(x)
+
+        # encoder: learning $q_\phi (z|x)$ where z is the generated latent and x is a datapoint
+        x = nn.Conv(kernel_size=(3, 3), strides=(2, 2), features=32, kernel_init=self.kernel_init, bias_init=self.bias_init)(x)
         x = nn.relu(x)
-        z_mean = nn.Dense(features=self.num_latents)(x)
-        z_logvar = nn.Dense(features=self.num_latents)(x)
+        x = nn.Conv(kernel_size=(3, 3), strides=(2, 2), features=64, kernel_init=self.kernel_init, bias_init=self.bias_init)(x)
+        x = nn.relu(x)
+        x = jnp.mean(x, axis=(1, 2))
+        z_mean = nn.Dense(features=self.num_latents, kernel_init=self.kernel_init, bias_init=self.bias_init)(x)
+        z_logvar = nn.Dense(features=self.num_latents, kernel_init=self.kernel_init, bias_init=self.bias_init)(x)
         return z_mean, z_logvar
+
+
+# class Decoder(nn.Module):
+
+#     kernel_init: Callable = nn.initializers.normal(stddev=0.01,)
+#     bias_init: Callable = nn.initializers.normal(stddev=0.01,)
+    
+#     @nn.compact
+#     def __call__(self, z):
+#         # decoder: learning $p_\theta (x|z)$ where z is a latent and x is a generated sample
+#         z = nn.Dense(features=500, name="fc1", kernel_init=self.kernel_init, bias_init=self.bias_init)(z)
+#         z = nn.relu(z)
+#         out = nn.Dense(784, name="fc2", kernel_init=self.kernel_init, bias_init=self.bias_init)(z)
+
+#         return out
+
+
+# class Encoder(nn.Module):
+#     num_latents: int = 5
+
+#     kernel_init: Callable = nn.initializers.normal(stddev=0.01,)
+#     bias_init: Callable = nn.initializers.normal(stddev=0.01,)
+
+#     @nn.compact
+#     def __call__(self, x):
+#         x = nn.Dense(500, name="fc1", kernel_init=self.kernel_init, bias_init=self.bias_init)(x)
+#         x = nn.relu(x)
+
+#         # NOTE: Couldn't we just initialize them to what we expect? Ex: z_mean init is 0 kernel, 0 bias and z_logvar is 0 kernel, 1 bias?
+#         z_mean = nn.Dense(features=self.num_latents, kernel_init=self.kernel_init, bias_init=self.bias_init)(x)
+#         z_logvar = nn.Dense(features=self.num_latents, kernel_init=self.kernel_init, bias_init=self.bias_init)(x)
+#         return z_mean, z_logvar
 
 
 class VAE(nn.Module):
